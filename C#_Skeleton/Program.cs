54326a7a9a8ef7a262cb4cca5809d2a1f956c866
zeynepaltunel <zeynepaltunel23@gmail.com>
2023-08-08 16:35:27 +0300

    I added a code describing the structure of C#

---
+++ C#_Skeleton/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace YourNamespace
+{   
+    class YourClass
+    {
+
+    } 
+    struct YourStruct
+    {
+
+    }
+    interface IYourInterface
+    {
+
+    }
+    enum YourEnum
+    {
+
+    }
+    
+    namespace YourNestedNamespace
+    {   
+        struct YourStruct
+        {
+        }
+    }
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            //The program starts here...
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
+
+
+
+
+
+
+
+
