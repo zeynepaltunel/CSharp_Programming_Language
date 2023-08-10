153f0520674b50ff07dbd845d04e5b1540845d48
zeynepaltunel <zeynepaltunel23@gmail.com>
2023-08-10 11:54:05 +0300

    I modified conditional statements

--- ConditionalStatemens/Program.cs
+++ ConditionalStatemens/Program.cs
@@ -11,6 +11,32 @@ namespace ConditionalStatements
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Enter a number");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            if (x%2 == 0)
+            {
+                Console.WriteLine("Your number is even"); // if your condition is true, this block works
+            }
+            else
+            {
+                Console.WriteLine("Your number is odd"); // if your condition is false, this block works
+            }
+
+            Console.WriteLine("Enter an integer number:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (y > 0) {
+                Console.WriteLine("Your entered number is positive");
+            }
+            else if (y < 0)
+            {
+                Console.WriteLine("Your entered number is negative");
+            }
+            else
+            {
+                Console.WriteLine("Your entered number is neutral");
+            }
         }
     }
 }
