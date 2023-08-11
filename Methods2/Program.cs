a65ff6870d6827001a42b7364287745d64da759a
zeynepaltunel <zeynepaltunel23@gmail.com>
2023-08-11 20:41:45 +0300

    I added more examples for Methods

---
+++ Methods2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = isEven(23);
+            Console.WriteLine(a);
+        }
+
+        static bool isEven(int num) {
+
+            if (num%2 == 0) 
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
