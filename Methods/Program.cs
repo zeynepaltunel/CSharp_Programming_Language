dd8ae529af68435fb80941c702aa43ca9a2fabdc
zeynepaltunel <zeynepaltunel23@gmail.com>
2023-08-11 20:40:56 +0300

    I added examples for Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+   
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A method is a block of code which only runs when it is called.
+            //Why use methods? To reuse code: define the code once, and use it many times.
+        }
+
+        #region method that returns no value, takes no parameters
+
+        public void Method1()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        #endregion
+
+        #region method that returns no value, takes a parameter
+
+        public void Method2(int x)
+        {
+            Console.WriteLine(x);
+        }
+
+        #endregion
+
+        #region method that returns a value, takes no parameters
+
+        public int Method3()
+        {
+            return 3;
+        }
+
+        #endregion
+
+        #region method that returns a value, takes a parameter
+
+        public int Method4(int x, int y)
+        {
+            return x + y;
+        }
+
+        #endregion
+
+        
+    }
+}
+
