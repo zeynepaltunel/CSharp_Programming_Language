55977e7f86bce03a564a6135d42f0ba4764e7821
zeynepaltunel <zeynepaltunel23@gmail.com>
2023-08-11 20:42:17 +0300

    I added examples for References

---
+++ References/Example.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace References
+{
+    internal class Example
+    {
+        //A class is a template for objects, and an object is an instance of a class.
+    }
+}
---
+++ References/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace References
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Example example;
+            //example = new Example();
+
+            //Example example = new Example();
+
+            Example x = new Example();
+            Example y = x;
+
+            if (x.Equals(y))
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not equal");
+            }
+
+            Console.ReadKey(); 
+        }
+    }
+}
