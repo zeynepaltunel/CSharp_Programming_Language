cd822b11b6038cf41ecfe22e5017ca64b7198ceb
zeynepaltunel <zeynepaltunel23@gmail.com>
2023-08-12 19:40:33 +0300

    I added examples of Accessing Methods Via Object

---
+++ MethodsAndObject/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MethodsAndObject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Math math = new Math();
+            int result = math.sum(7, 8);
+            MessageBox.Show(result.ToString());
+        }
+    }
+}
---
+++ MethodsAndObject/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObject
+{
+    class Math
+    {
+        public int sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public double division(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+
+}
