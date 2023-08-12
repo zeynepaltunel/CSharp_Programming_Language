c45a963efaaec91e183a1b8e887dc670d3fa51d4
zeynepaltunel <zeynepaltunel23@gmail.com>
2023-08-13 01:20:30 +0300

    I added examples for method overloading

---
+++ Overload/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    public class Math
+    {
+        /* Multiple methods can have the same name with
+           different parameters with method overloading. */
+
+        public int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public double sum(double a, double b) 
+        { 
+            return a + b; 
+        }
+
+    }
+}
---
+++ Overload/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine(math.sum(2,3,4));
+            Console.WriteLine(math.sum(5,7));
+            Console.WriteLine(math.sum(12.2,23.2));
+        }
+    }
+}
