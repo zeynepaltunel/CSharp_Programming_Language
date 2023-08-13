4095e91c89df6381965ddb85578f510617477d14
zeynepaltunel <zeynepaltunel23@gmail.com>
2023-08-13 20:15:22 +0300

    I added examples for constructors and static constructors

--- ClassMember/Program.cs
+++ ClassMember/Program.cs
@@ -10,11 +10,16 @@ namespace ClassMember
     {
         static void Main(string[] args)
         {
-            Example example = new Example();
-            Console.WriteLine(example.Name);
-            example.Name = "Test";
-            Console.WriteLine(example.Name);
-            example.Number = 1;
+            //Example example = new Example();
+            //Console.WriteLine(example.Name);
+            //example.Name = "Test";
+            //Console.WriteLine(example.Name);
+            //example.Number = 1;
+
+            new Example();
+            new Example();
+            new Example();
+            new Example();
         }
     }
 
@@ -42,33 +47,58 @@ namespace ClassMember
         //    }
         //}
 
-        string x;
+        //string x;
 
         // Ctrl + R + E
 
-        public string X { get => x; set => x=value; }
+        //public string X { get => x; set => x=value; }
 
         #endregion
 
         #region Property 2
 
         //prop
-        public int Number { get; set; }
+        //public int Number { get; set; }
 
         #endregion
 
-        #region Auto Propert Initializers
+        #region Auto Property Initializers
 
-        public string Name { get; set; } = "Zeynep";
+        //public string Name { get; set; } = "Zeynep";
 
         #endregion
 
         #region Constructor
 
+        //A constructor is a special method that is used to initialize objects.
+        //Constructor method must be public.
+        //Constructor method has no return value
+        //Constructor method name must be same as class name
+
+        public int X { get; set; }
+
+        public Example() 
+        {
+            Console.WriteLine("Object created");
+        }
+
+        public void example1()
+        {
+
+        }
 
         #endregion
 
         #region Static Constructor
+
+        /*  Runs before normal constructor.
+            Triggered from the first of the object requests made
+            from the related class, not from the next ones. */
+
+        static Example()
+        {
+            Console.WriteLine("Worked before constructor");
+        }
 
         #endregion
     }
