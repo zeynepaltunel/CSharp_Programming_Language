61aac026dcb5266c97e6ed62bb2bf98fe1699df3
zeynepaltunel <zeynepaltunel23@gmail.com>
2023-08-30 23:30:42 +0300

    I added examples for sealed class

---
+++ SealedVsFinalClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedVsFinalClass
+{
+    class Program
+    {
+        sealed class Class2
+        {
+            
+        }
+        class Class1 : Class2
+        {
+            
+        }
+
+        //in c#, there is no keyword like 'final'.
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
