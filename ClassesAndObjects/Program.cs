8c3ed4f412669d6fdbd695d521011b3ead9141da
zeynepaltunel <zeynepaltunel23@gmail.com>
2023-12-29 22:35:05 +0300

    Modified project file.

---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine();
+        }
+    }
+    class Car
+    {
+        string color = "white";
+    }
+}
